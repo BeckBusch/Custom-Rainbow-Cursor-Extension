d183f682facb311b4459d51892f0d03f969fd269
Beck Busch <beckbusch5@gmail.com>
2016-09-05 13:15:41 +1200

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,8 @@
+chrome.browserAction.onClicked.addListener(function(tab){
+    javascript:var element = document.createElement("link");
+element.setAttribute("rel", "stylesheet");
+element.setAttribute("type", "text/css");
+element.setAttribute("href", "cursor.css");
+document.getElementsByTagName("head")[0].appendChild(element);
+}
+);
