3fd640227414fb71a8024ff45e2fce69ae39706b
Beck Busch <beckbusch5@gmail.com>
2016-09-09 12:03:11 +1200

    Update options.js

--- options.js
+++ options.js
@@ -1,10 +1,8 @@
 // Saves options to chrome.storage
 function save_options() {
-  var color = document.getElementById('color').value;
-  var likesColor = document.getElementById('like').checked;
+  var cursor = document.getElementById('cursor').value;
   chrome.storage.sync.set({
-    favoriteColor: color,
-    likesColor: likesColor
+    favoriteCursor: cursor
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
@@ -20,11 +18,9 @@ function save_options() {
 function restore_options() {
   // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get({
-    favoriteColor: 'red',
-    likesColor: true
+    favoriteCursor: 'red',
   }, function(items) {
-    document.getElementById('color').value = items.favoriteColor;
-    document.getElementById('like').checked = items.likesColor;
+    document.getElementById('cursor').value = items.favoriteCursor;
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
