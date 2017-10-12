a213f3022ab66d473aba07d578b3f0ec6e5608ee
Beck Busch <beckbusch5@gmail.com>
2017-10-12 22:23:12 +1300

    add session analytics tracking

--- eventPage.js
+++ eventPage.js
@@ -1,4 +1,4 @@
-ignoreUpdate = ["3.3.0", "3.3.1"]
+ignoreUpdate = ["3.3.0", "3.3.1", "3.3.2"]
 
 chrome.runtime.onInstalled.addListener(function(details){
     if(details.reason == "install"){
@@ -17,3 +17,13 @@ chrome.runtime.onInstalled.addListener(function(details){
 
     }
 });
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-98900189-4']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
