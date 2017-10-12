f51efeafa5daf2d9028ccec814ef875e414e491a
Beck Busch <beckbusch5@gmail.com>
2017-10-12 18:35:14 +1300

    added quiet updating
    Quite updates do not trigger the information page to load. this way, I can push bug updates and small features without annoying users by opening update information pages

--- eventPage.js
+++ eventPage.js
@@ -1,4 +1,4 @@
-
+ignoreUpdate = ["3.3.0", "3.3.1"]
 
 chrome.runtime.onInstalled.addListener(function(details){
     if(details.reason == "install"){
@@ -7,8 +7,13 @@ chrome.runtime.onInstalled.addListener(function(details){
         chrome.runtime.openOptionsPage();
 
     }else if(details.reason == "update"){
+        var thisVersion = chrome.runtime.getManifest().version;
+        if  (ignoreUpdate.indexOf(thisVersion) == -1) {
+            window.open("http://beckbusch.github.io/Custom-Rainbow-Cursor-Extension/?type=update");
+            chrome.runtime.openOptionsPage();
+        }
 
-        window.open("http://beckbusch.github.io/Custom-Rainbow-Cursor-Extension/?type=update");
-        chrome.runtime.openOptionsPage();
+
+
     }
 });
