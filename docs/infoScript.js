709d100a75edec0110b63e2bd9346742ad1ea310
Beck Busch <beckbusch5@gmail.com>
2017-10-16 13:24:52 +1300

    worked on the information website

--- docs/infoScript.js
+++ docs/infoScript.js
@@ -1,3 +1,37 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("home").style.display = "initial";
+}, false);
+
+function home(){
+    document.getElementById("home").style.display = "initial";
+    document.getElementById("update").style.display = "none";
+    document.getElementById("contact").style.display = "none";
+    document.getElementById("gallery").style.display = "none";
+}
+
+function gallery(){
+    document.getElementById("gallery").style.display = "initial";
+    document.getElementById("update").style.display = "none";
+    document.getElementById("contact").style.display = "none";
+    document.getElementById("home").style.display = "none";
+}
+
+function update(){
+    document.getElementById("update").style.display = "initial";
+    document.getElementById("contact").style.display = "none";
+    document.getElementById("home").style.display = "none";
+    document.getElementById("gallery").style.display = "none";
+}
+
+function contact(){
+    document.getElementById("contact").style.display = "initial";
+    document.getElementById("home").style.display = "none";
+    document.getElementById("update").style.display = "none";
+    document.getElementById("gallery").style.display = "none";
+}
+
+
 function getParameterByName(name, url) {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, "\\$&");
