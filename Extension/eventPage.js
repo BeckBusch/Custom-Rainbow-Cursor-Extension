dad3a62c645dfd8a03be5437a855b72e79130fa3
Beck Busch <beckbusch5@gmail.com>
2017-10-18 09:32:15 +1300

    chnaged info page

--- Extension/eventPage.js
+++ Extension/eventPage.js
@@ -21,7 +21,13 @@ chrome.runtime.onInstalled.addListener(function(details){
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-98900189-4']);
 _gaq.push(['_trackPageview']);
-
+_gaq.push(['_setCustomVar',
+    4,                // This custom var is set to slot #1.  Required parameter.
+    'UpdateVersion',    // The name of the custom variable.  Required parameter.
+    chrome.runtime.getManifest().version,        // The value of the custom variable.  Required parameter.
+                      //  (possible values might be Free, Bronze, Gold, and Platinum)
+    2                 // Sets the scope to session-level.  Optional parameter.
+]);
 (function() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
     ga.src = 'https://ssl.google-analytics.com/ga.js';
